ce95be8cb1968eb7544b318fd3a57d6ccb0e95cb
Marcel Fahle <m.fahle@gmail.com>
2018-11-11 23:49:54 +0100

    cal

--- src/components/kalender/kalender.js
+++ src/components/kalender/kalender.js
@@ -198,7 +198,7 @@ export default class Kalender extends PureComponent {
     const months = this.allMonths()
 
     const currentMonthDate =
-      months[this.state.current].node &&
+      months[this.state.current] &&
       moment(months[this.state.current].node.startDate)
     const lastMonth = Moment().subtract('month', 1)
     const currentRange = currentMonthDate.range('month')
